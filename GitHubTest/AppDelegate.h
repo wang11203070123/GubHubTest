9b740be0d5f78b2ce4bfb776e18b2e9900c7890b
子叶 <861645007@qq.com>
2014-12-15 10:21:28 +0800

    Initial Commit

---
+++ GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by wanghuanqiang on 14/12/15.
+//  Copyright (c) 2014年 王焕强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubTest
+//
+//  Created by wanghuanqiang on 14/12/15.
+//  Copyright (c) 2014年 王焕强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
